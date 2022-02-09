f7d7f73cb4b7054553590e6f35be6da4b5d1ad96
Jason-Wanxt <61458343+Jason-Wanxt@users.noreply.github.com>
2022-02-09 14:22:17 +0800

    Update withdraw-test.js

--- packages/arb-bridge-eth/test/withdraw-test.js
+++ packages/arb-bridge-eth/test/withdraw-test.js
@@ -1,8 +1,7 @@
 const { expect } = require("chai");
 const { Contract } = require("ethers");
 const { ethers,hre } = require("hardhat");
-
-
+const TestCase = require("./Outbox_testcase.json")
 async function sendEth(send_account, to_address, send_token_amount) {
     const nonce = await ethers.provider.getTransactionCount(send_account, "latest");
     const gas_price = await ethers.provider.getGasPrice();
@@ -25,72 +24,71 @@ async function getTokenTransferData(to_address, send_token_amount, erc20) {
     return iface.encodeFunctionData("transfer", [to_address, send_token_amount]);
 }
 
+async function setEntries(cases, outbox) {
+    let batchNumMap = new Map();
+    const length = cases.length;
+    for(let i = 0; i < length; i++) {
+        if(!batchNumMap.has(cases[i].batchNum)) {
+            batchNumMap.set(cases[i].batchNum, cases[i].root);
+            await outbox.setEntry(cases[i].batchNum, cases[i].root);
+        }
+    }  
+}
+
 describe("Outbox", async function () {
     let outboxWithOpt;
     let outboxWithoutOpt;
     let token;
+    let bridge;
+    const cases = TestCase.cases;
+    const sentEthAmount = ethers.utils.parseEther("10");
+    
     before(async function () {
-        const OutboxWithOpt = await ethers.getContractFactory("OutboxWithOpt");
-        const OutboxWithoutOpt = await ethers.getContractFactory("OutboxWithoutOpt");
+        const accounts = await ethers.getSigners();
+        const OutboxWithOpt = await ethers.getContractFactory("OutboxWithOptTester");
+        const OutboxWithoutOpt = await ethers.getContractFactory("OutboxWithoutOptTester");
         const Token = await ethers.getContractFactory("TestToken");
+        const Bridge = await ethers.getContractFactory("Bridge");
         token = await Token.deploy("Test token", "tst");
         outboxWithOpt = await OutboxWithOpt.deploy();
         outboxWithoutOpt = await OutboxWithoutOpt.deploy();
+        bridge = await Bridge.deploy();
         // await outboxWithOpt.deployed();
         // await outboxWithoutOpt.deployed();
         const balance = await token.balanceOf("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-        console.log("balance of this token:" + balance);
-        await outboxWithOpt.initialize("0x95F2e4096482Ebaded815b3aAcFA1524EF3E0568","0x95F2e4096482Ebaded815b3aAcFA1524EF3E0568");
-        await outboxWithoutOpt.initialize("0x95F2e4096482Ebaded815b3aAcFA1524EF3E0568","0x95F2e4096482Ebaded815b3aAcFA1524EF3E0568");
+        
+        await outboxWithOpt.initialize("0xC12BA48c781F6e392B49Db2E25Cd0c28cD77531A", bridge.address);
+        await outboxWithoutOpt.initialize("0xC12BA48c781F6e392B49Db2E25Cd0c28cD77531A", bridge.address);
+
+        await bridge.setOutbox(outboxWithOpt.address, true);
+        await bridge.setOutbox(outboxWithoutOpt.address, true);
+
+        await setEntries(cases, outboxWithOpt);
+        await setEntries(cases, outboxWithoutOpt);
+
+        await sendEth(accounts[0].address, bridge.address, sentEthAmount);
+        
+        
     })
     it("First call to initial some storage", async function () {    
-        expect(await outboxWithOpt.executeTransaction("1",["0x5fd1d356f1e53db956eec1c68a4fb02585a3b7a1d78dfbf1f4d0fad9060422d5"],"1","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","100","100","1643814360","0","0x")).to.emit("BridgeCallTriggered")
-        expect(await outboxWithoutOpt.executeTransaction("1",["0x5fd1d356f1e53db956eec1c68a4fb02585a3b7a1d78dfbf1f4d0fad9060422d5"],"1","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","100","100","1643814360","0","0x")).to.emit("BridgeCallTriggered")
+        expect(await outboxWithOpt.executeTransaction(cases[0].args.batchNum, cases[0].args.proof, cases[0].args.index, cases[0].args.l2Sender, cases[0].args.destAddr, cases[0].args.l2Block, cases[0].args.l1Block, cases[0].args.l2Timestamp, cases[0].args.amount, cases[0].args.calldataForL1)).to.emit("BridgeCallTriggered")
+        expect(await outboxWithoutOpt.executeTransaction(cases[0].args.batchNum, cases[0].args.proof, cases[0].args.index, cases[0].args.l2Sender, cases[0].args.destAddr, cases[0].args.l2Block, cases[0].args.l1Block, cases[0].args.l2Timestamp, cases[0].args.amount, cases[0].args.calldataForL1)).to.emit("BridgeCallTriggered")
         
     });
     
     it("Call twice without storage initail cost", async function () {
-        expect(await outboxWithOpt.executeTransaction("1",["0x5fd1d356f1e53db956eec1c68a4fb02585a3b7a1d78dfbf1f4d0fad9060422d5"],"1","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","100","100","1643814360","0","0x")).to.emit("BridgeCallTriggered")
-        expect(await outboxWithoutOpt.executeTransaction("1",["0x5fd1d356f1e53db956eec1c68a4fb02585a3b7a1d78dfbf1f4d0fad9060422d5"],"1","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","100","100","1643814360","0","0x")).to.emit("BridgeCallTriggered")
+        expect(await outboxWithOpt.executeTransaction(cases[1].args.batchNum, cases[1].args.proof, cases[1].args.index, cases[1].args.l2Sender, cases[1].args.destAddr, cases[1].args.l2Block, cases[1].args.l1Block, cases[1].args.l2Timestamp, cases[1].args.amount, cases[1].args.calldataForL1)).to.emit("BridgeCallTriggered")
+        expect(await outboxWithoutOpt.executeTransaction(cases[1].args.batchNum, cases[1].args.proof, cases[1].args.index, cases[1].args.l2Sender, cases[1].args.destAddr, cases[1].args.l2Block, cases[1].args.l1Block, cases[1].args.l2Timestamp, cases[1].args.amount, cases[1].args.calldataForL1)).to.emit("BridgeCallTriggered")
         
     });
 
-    it("call to send eth", async function() {
-        const accounts = await ethers.getSigners();
-        const sentEthAmount = ethers.utils.parseEther("1");
+    it("should revert when use redo the same tx", async function () {
+        await expect( outboxWithOpt.executeTransaction(cases[0].args.batchNum, cases[0].args.proof, cases[0].args.index, cases[0].args.l2Sender, cases[0].args.destAddr, cases[0].args.l2Block, cases[0].args.l1Block, cases[0].args.l2Timestamp, cases[0].args.amount, cases[0].args.calldataForL1)).to.be.revertedWith("ALREADY_SPENT")
+        await expect( outboxWithoutOpt.executeTransaction(cases[0].args.batchNum, cases[0].args.proof, cases[0].args.index, cases[0].args.l2Sender, cases[0].args.destAddr, cases[0].args.l2Block, cases[0].args.l1Block, cases[0].args.l2Timestamp, cases[0].args.amount, cases[0].args.calldataForL1)).to.be.revertedWith("ALREADY_SPENT")
         
-        const destAddress = "0x760723CD2e632826c38Fef8CD438A4CC7E7E1A40";
-        await sendEth(accounts[0].address, outboxWithOpt.address, sentEthAmount);
-        await sendEth(accounts[0].address, outboxWithoutOpt.address, sentEthAmount);
-        const testBytes = ethers.utils.keccak256("0x");
-        // //await OutboxWithoutOptInitailTx.wait();
-        await outboxWithOpt.executeTransaction("1",[testBytes],"1",accounts[0].address,destAddress,"100","100","1643814360", sentEthAmount,"0x")
-        await outboxWithoutOpt.executeTransaction("1",[testBytes],"1",accounts[0].address,destAddress,"100","100","1643814360", sentEthAmount,"0x")
-        const balance = await ethers.provider.getBalance(destAddress);
-        const shouldReceivedAmount = await sentEthAmount.mul(2).toString();
-        expect(balance).equals(shouldReceivedAmount)
-    })
+    });
 
-    it("call to send token", async function() {
-        const accounts = await ethers.getSigners();
-        const sentTokenAmount = ethers.utils.parseEther("1");
-        await token.mint(accounts[0].address, ethers.utils.parseEther("100"))
-        const destAddress = "0x760723CD2e632826c38Fef8CD438A4CC7E7E1A40";
-        await token.transfer(outboxWithOpt.address, sentTokenAmount);
-        await token.transfer(outboxWithoutOpt.address, sentTokenAmount);
-        console.log("here")
-        const balance1 = await token.balanceOf(outboxWithOpt.address);
-        console.log(balance1)
-        const testBytes = ethers.utils.keccak256("0x");
-        const txData = await getTokenTransferData(destAddress, sentTokenAmount, token);
-        console.log(txData);
-        // //await OutboxWithoutOptInitailTx.wait();
-        await outboxWithOpt.executeTransaction("1",[testBytes],"1",accounts[0].address, token.address,"100","100","1643814360", 0, txData)
-        await outboxWithoutOpt.executeTransaction("1",[testBytes],"1",accounts[0].address, token.address,"100","100","1643814360", 0, txData)
-        const balance = await token.balanceOf(destAddress);
-        const shouldReceivedAmount = await sentTokenAmount.mul(2).toString();
-        expect(balance).equals(shouldReceivedAmount)
-    })
+    
     
   });
   
